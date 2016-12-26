41c4b11971d2a65ba219d23eb7c24ac8a7bb9afb
Kevin Gurton <kevin.gurton@gmail.com>
2016-12-26 13:10:57 +0000

    Made name editable

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,7 +10,10 @@ export class Hero {
   template: `<h1>{{title}}</h1>
              <h2>{{hero.name}} details</h2>
              <div><label>id: </label>{{hero.id}}</div>
-             <div><label>name: </label>{{hero.name}}</div>`
+             <div>
+               <label>name: </label>
+               <input value="{{hero.name}}" placeholder="name">
+             </div>`
 })
 export class AppComponent  {
    title = 'Tour of Heroes';
