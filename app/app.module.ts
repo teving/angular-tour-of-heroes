62c7a0c2e248b405b4f9191947420a728c8421d3
Kevin Gurton <kevin.gurton@gmail.com>
2016-12-27 15:50:43 +0000

    Moved old app component to heroes component

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,7 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
+
+import { HeroService } from './hero.service';
 
 @NgModule({
    imports: [
@@ -12,8 +15,14 @@ import { HeroDetailComponent }  from './hero-detail.component';
    ],
    declarations: [
       AppComponent,
+      HeroesComponent,
       HeroDetailComponent
    ],
-   bootstrap:    [ AppComponent ]
+   bootstrap: [
+      AppComponent
+   ],
+   providers: [
+      HeroService
+   ]
 })
 export class AppModule { }
