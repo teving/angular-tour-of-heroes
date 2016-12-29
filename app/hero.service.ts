4d8c3757b58613ab671ecaa68a6a787af0c7e083
Kevin Gurton <kevin.gurton@gmail.com>
2016-12-29 21:49:02 +0000

    Added put method for heroes

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -6,6 +6,7 @@ import { Hero } from './hero';
 @Injectable()
 export class HeroService {
    private heroesUrl = 'api/heroes';
+   private headers = new Headers({'Content-Type': 'application/json'});
 
    constructor(private http: Http) { }
 
@@ -24,7 +25,16 @@ export class HeroService {
                .then(response => response.json().data as Hero)
                .catch(this.handleError);
    }
-   
+
+   update(hero: Hero): Promise<Hero> {
+      const url = `${this.heroesUrl}/${hero.id}`;
+      return this.http
+               .put(url, JSON.stringify(hero), {headers: this.headers})
+               .toPromise()
+               .then(() => hero)
+               .catch(this.handleError);
+   }
+
    private handleError(error:any): Promise<any>{
       console.error('An error occurred', error);
       return Promise.reject(error.message || error);
