6b931b7a621e7132e0f4c3a19e19719388b12a55
Kevin Gurton <kevin.gurton@gmail.com>
2016-12-30 17:46:55 +0000

    Added hero search component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,3 +1,5 @@
+import './rxjs-extensions';
+
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
